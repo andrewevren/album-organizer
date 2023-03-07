2dfb1e34ffb1f5d068f4f517198d946ec153399d
Andrew Evren <andrewevren@gmail.com>
2023-03-06 18:25:21 -0600

    Import album-art module, declare type, implement search

---
+++ src/album-art.d.ts
@@ -0,0 +1,3 @@
+declare module 'album-art' {
+    export default function albumArt( artist: string, options?: object, callback?: function): Promise;
+}
--- src/app/sidebar/sidebar.tsx
+++ src/app/sidebar/sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import albumArt from 'album-art';
 import styles from './sidebar.module.scss';
 
 /* eslint-disable-next-line */
@@ -6,6 +7,7 @@ export interface SidebarProps {}
 
 export function Sidebar(props: SidebarProps) {
   const [barOpen, setBarOpen] = useState(true)
+  const [newAlbum, setNewAlbum] = useState('')
   const [formData, setFormData] = useState({
     artist: '', album: ''
   })
@@ -19,8 +21,12 @@ export function Sidebar(props: SidebarProps) {
     setFormData({...formData, [name]: value})
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    await albumArt(formData.artist, {album:formData.album, size:'medium'})
+      .then((response:string) => setNewAlbum(response))
+
     setFormData({artist: '', album: ''})
   }
 
@@ -36,7 +42,11 @@ export function Sidebar(props: SidebarProps) {
               <input className={styles['input']} type='text' placeholder='Album' name='album' value={formData.album} onChange={handleChange}/>
               <button className={styles['search-submit']} type='submit'>Submit</button>
             </form>
-            <div className={styles['new-album-box']}></div>
+            <div className={styles['new-album-box']}>
+              {newAlbum !== '' &&
+                <img src={newAlbum} alt='Album art' className={styles['new-album-art']} />
+              }
+            </div>
           </div>
           <div>
             <input className={styles['input']} type='text' placeholder='Name'/>
